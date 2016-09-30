9031d3a9ad62f20c6fa3859cd8fdd2908b2a7e35
chulanovskyi <skyrocker@mail.ru>
2016-09-30 15:47:43 +0300

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # 9_github_trending
+
+###Prerequisites:
+
+Run in console `pip install -r requirements.txt` to install 3rd party modules.
+
+---
+
+Script will help you to track most starred repositories on github.com created from last week.
+Of course script need available internet connection.
+
+###How to use:
+
+You can edit `REPOS_TO_GET` constant to change how many top repositories will be printed.
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,31 @@
+import datetime
+import requests
 
 
-def get_trending_repositories(top_size):
-    pass
+REPOS_TO_GET = 20
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(top_size, from_date):
+    last_week_repos = requests.get(
+        'https://api.github.com/search/repositories?q=created:>={a_date}&sort=stars'.format(
+            a_date=from_date)).json()
+    return last_week_repos['items'][:top_size]
+    
+
+def get_last_week():
+    today = datetime.date.today()
+    week_earlier = today - datetime.timedelta(days=7)
+    return week_earlier
+
+
+def pprint_repo(repo):
+    print('Stars: {stars} | Issues: {issues}\n{repo_link}\n'.format(
+        stars=repo['stargazers_count'],
+        issues=repo['open_issues'],
+        repo_link=repo['html_url']))
+
 
 if __name__ == '__main__':
-    pass
+    repos = get_trending_repositories(REPOS_TO_GET, get_last_week())
+    for repo in repos:
+        pprint_repo(repo)
